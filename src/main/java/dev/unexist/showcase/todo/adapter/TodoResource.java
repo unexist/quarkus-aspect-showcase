00055edd049c6c19fcccce8b322a257e6673b9d9
Christoph Kappel <christoph@unexist.dev>
2021-10-03 15:54:55 +0200

    Imported initially

---
+++ src/main/java/dev/unexist/showcase/todo/adapter/TodoResource.java
@@ -0,0 +1,151 @@
+/**
+ * @package Showcase-AOP-Micronaut
+ *
+ * @file Todo resource
+ * @copyright 2021 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.adapter;
+
+import dev.unexist.showcase.todo.domain.todo.Todo;
+import dev.unexist.showcase.todo.domain.todo.TodoBase;
+import dev.unexist.showcase.todo.domain.todo.TodoService;
+import io.micronaut.context.annotation.Context;
+import io.micronaut.context.annotation.Parameter;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.annotation.Put;
+import io.micronaut.http.uri.UriMatchInfo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.inject.Inject;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@Controller("/todo")
+public class TodoResource {
+
+    @Inject
+    TodoService todoService;
+
+    @Post(consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+    @Operation(summary = "Create new todo")
+    @Tag(name = "Todo")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Todo created"),
+            @ApiResponse(responseCode = "406", description = "Bad data"),
+            @ApiResponse(responseCode = "500", description = "Server error")
+    })
+    public HttpResponse<?> create(TodoBase base, @Context UriMatchInfo uriInfo) {
+        HttpResponse<?> response;
+
+        if (this.todoService.create(base)) {
+            URI uri = URI.create(uriInfo.getUri());
+
+            response = HttpResponse.created(uri);
+        } else {
+            response = HttpResponse.status(HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return response;
+    }
+
+    @Get
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Get all todos")
+    @Tag(name = "Todo")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "List of todo", content =
+                @Content(array = @ArraySchema(schema = @Schema(implementation = Todo.class)))),
+            @ApiResponse(responseCode = "204", description = "Nothing found"),
+            @ApiResponse(responseCode = "500", description = "Server error")
+    })
+    public HttpResponse<?> getAll() {
+        List<Todo> todoList = this.todoService.getAll();
+
+        HttpResponse<?> response;
+
+        if (todoList.isEmpty()) {
+            response = HttpResponse.noContent();
+        } else {
+            response = HttpResponse.ok(todoList);
+        }
+
+        return response;
+    }
+
+    @Get(value = "{id}", produces = MediaType.APPLICATION_JSON)
+    @Operation(summary = "Get todo by id")
+    @Tag(name = "Todo")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Todo found", content =
+                @Content(schema = @Schema(implementation = Todo.class))),
+            @ApiResponse(responseCode = "404", description = "Todo not found"),
+            @ApiResponse(responseCode = "500", description = "Server error")
+    })
+    public HttpResponse<?> findById(@Parameter("id") int id) {
+        Optional<Todo> result = this.todoService.findById(id);
+
+        HttpResponse<?> response;
+
+        if (result.isPresent()) {
+            response = HttpResponse.ok(result.get());
+        } else {
+            response = HttpResponse.status(HttpStatus.NOT_FOUND);
+        }
+
+        return response;
+    }
+
+    @Put(value = "{id}", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+    @Operation(summary = "Update todo by id")
+    @Tag(name = "Todo")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Todo updated"),
+            @ApiResponse(responseCode = "404", description = "Todo not found"),
+            @ApiResponse(responseCode = "500", description = "Server error")
+    })
+    public HttpResponse<?> update(@Parameter("id") int id, TodoBase base) {
+        HttpResponse<?> response;
+
+        if (this.todoService.update(id, base)) {
+            response = HttpResponse.noContent();
+        } else {
+            response = HttpResponse.status(HttpStatus.NOT_FOUND);
+        }
+
+        return response;
+    }
+
+    @Delete(value = "{id}", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+    @Operation(summary = "Delete todo by id")
+    @Tag(name = "Todo")
+    public HttpResponse<?> delete(@Parameter("id") int id) {
+        HttpResponse<?> response;
+
+        if (this.todoService.delete(id)) {
+            response = HttpResponse.noContent();
+        } else {
+            response = HttpResponse.status(HttpStatus.NOT_FOUND);
+        }
+
+        return response;
+    }
+}
---
+++ src/main/java/dev/unexist/showcase/todo/application/Application.java
@@ -0,0 +1,56 @@
+/**
+ * @package Showcase
+ *
+ * @file Main application
+ * @copyright 2021 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id\$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.application;
+
+import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.ServerVariable;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Todo",
+                version = "0.1",
+                description = "Todo API",
+                license = @License(name = "Apache 2.0", url = "https://unexist.dev"),
+                contact = @Contact(url = "https://unexist.dev", name = "Christoph Kappel", email = "christoph@unexist.dev")
+        ),
+        tags = {
+                @Tag(name = "Todo", description = "desc", externalDocs = @ExternalDocumentation(description = "docs desc")),
+        },
+        externalDocs = @ExternalDocumentation(description = "definition docs desc"),
+        security = {
+                @SecurityRequirement(name = "req 1", scopes = {"a", "b"}),
+                @SecurityRequirement(name = "req 2", scopes = {"b", "c"})
+        },
+        servers = {
+                @Server(
+                        description = "server 1",
+                        url = "http://foo",
+                        variables = {
+                                @ServerVariable(name = "var1", description = "var 1", defaultValue = "1", allowableValues = {"1", "2"}),
+                                @ServerVariable(name = "var2", description = "var 2", defaultValue = "1", allowableValues = {"1", "2"})
+                        })
+        }
+)
+public class Application {
+    @SuppressWarnings("checkstyle:UncommentedMain")
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
